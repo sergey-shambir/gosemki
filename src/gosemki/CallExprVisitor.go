83bec9977c6221e6009577e50d2bedea1b4c926c
Sergey Shambir <sshambir@yandex.ru>
2015-06-28 10:20:48 +0300

    Fixed highlighting regression for call expressions.

--- src/gosemki/CallExprVisitor.go
+++ src/gosemki/CallExprVisitor.go
@@ -7,11 +7,16 @@ import (
 type CallExprVisitor struct {
     indexer *PackageIndexer
     funName *ast.Ident
+    otherNodes []ast.Node
 }
 
 func (this *CallExprVisitor) InspectNode(node ast.Node) bool {
     switch x := node.(type) {
     case *ast.Ident:
+        oldNode := this.funName
+        if oldNode != nil && oldNode.Obj != nil && oldNode.Obj.Kind != ast.Bad {
+            this.indexer.AddIdentRange(this.funName)
+        }
         this.funName = x
         return false
     case *ast.KeyValueExpr:
@@ -25,6 +30,9 @@ func (this *CallExprVisitor) InspectNode(node ast.Node) bool {
 }
 
 func (this *CallExprVisitor) ApplyIdent() {
+    for _, v := range this.otherNodes {
+        ast.Inspect(v, this.indexer.InspectNode)
+    }
     if this.funName != nil {
         pos := this.indexer.fset.Position(this.funName.NamePos)
         goRange := GoRange {
